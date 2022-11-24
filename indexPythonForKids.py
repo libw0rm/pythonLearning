f91640ee9132dd9ad42d57d5297397e5079f3ca5
libw0rm <mpxweb@yahoo.com>
2022-11-24 19:34:54 +0600

    [iPFK] добавлены примеры кода главы "Списки мощнее строк"

---
+++ indexPythonForKids.py
@@ -0,0 +1,29 @@
+# !{39} Списки мощнее строк
+
+# wizzard_list_str = 'Паучьи лапки, жабий палец, глаз тритона, крыло летучей мыши, жир слизня, перхоть змеи'
+
+# print(wizzard_list_str)
+
+# wizzard_list = ['Паучьи лапки', 'жабий палец', 'глаз тритона', 'крыло летучей мыши', 'жир слизня', 'перхоть змеи']
+
+# print(wizzard_list[2])
+
+# wizzard_list[2] = 'язык улитки'
+# print(wizzard_list)
+# print(wizzard_list[2:5])
+# print(wizzard_list[0:2])
+
+# someNumbers = [1, 2, 5, 10, 20]
+# someStrings = ['Нож', 'отточен', 'точен', 'очень']
+# numbersAndStrings = [7, 'раз', 'отпей', 1, 'отъешь']
+# myList = [someNumbers, someStrings]
+# print(numbersAndStrings)
+# print(myList)
+
+#  !{41}
+numbers = [1, 2, 3, 4, 5]
+strings = ['хватит', 'циферки', 'считать']
+mySecondList = [numbers, strings]
+print(mySecondList)
+
+
